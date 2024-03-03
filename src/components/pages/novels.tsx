55b73a6c421fd47abdd40605cdce9717d1cf2c8d
TranDat1114 <cctalk1114@gmail.com>
2024-03-03 16:43:33 +0700

    Remove react-tabs dependency, update book description, and rename novels page to genres

--- src/components/pages/novels.tsx
+++ src/components/pages/novels.tsx
@@ -2,10 +2,10 @@ import { Link, useParams } from "react-router-dom";
 import React, { useEffect, useState } from 'react';
 import booksData from '@/data/books.json';
 import { Book } from '@/types/home.ts';
-import { ArrowDownWideNarrow, ArrowUpNarrowWide, Glasses, MoreHorizontal, NotebookText, ThumbsUp } from 'lucide-react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import {  Glasses, NotebookText, ThumbsUp } from 'lucide-react';
+
 import 'react-tabs/style/react-tabs.css';
-import userData from '@/data/user.json';
+// import userData from '@/data/user.json';
 import Ads from "@components/ui/banner/ads";
 import { useTranslation } from "react-i18next";
 import LoadingSpinner from '@components/pages/loading'
@@ -16,11 +16,11 @@ const NovelsPage = () => {
         keyPrefix: "main.novels"
     });
 
-    const [isUpNarrow, setIsUpNarrow] = useState(false);
+    // const [isUpNarrow, setIsUpNarrow] = useState(false);
 
-    const toggleIcon = () => {
-        setIsUpNarrow(!isUpNarrow);
-    };
+    // const toggleIcon = () => {
+    //     setIsUpNarrow(!isUpNarrow);
+    // };
 
     const { bookName } = useParams<{ bookName: string }>();
 
@@ -44,12 +44,14 @@ const NovelsPage = () => {
     const [isHideDetails, setIsHideDetails] = useState(true);
 
     if (!bookDetails) {
-        return <div><LoadingSpinner/></div>;
+        return <div><LoadingSpinner /></div>;
         // code Thêm trang loading ở đây
     }
 
+
+
     return (
-        <div className='space-y-4'>
+        <div className='space-y-4 w-full'>
             {loading ? (
                 <LoadingSpinner />
             ) : (
@@ -57,11 +59,7 @@ const NovelsPage = () => {
                     <div className={`hero rounded-box relative w-full my-4`}>
                         <img src={`${bookDetails.img}`} className="absolute w-full h-full object-cover rounded-box" alt={bookDetails.name + 'Cover'} />
                         <div className="hero-overlay bg-opacity-80 backdrop-blur-sm rounded-box"></div>
-                        {/* <div className="hero-content flex-col lg:flex-row lg:gap-x-12">
-                    <img src={`${bookDetails.img}`} className="max-w-sm rounded-lg shadow-2xl" alt={bookDetails.name + 'Cover'} />
-                    
-                </div> */}
-
+                   
                         <div className="hero-content flex-col lg:flex-row lg:gap-x-8 w-full justify-evenly">
                             <img src={bookDetails.img} className="shadow-2xl max-w-sm rounded-lg bg-transparent object-cover max-h-96" alt={bookDetails.name + 'Cover'} />
 
@@ -146,16 +144,16 @@ const NovelsPage = () => {
 
                         </div>
                     </div>
-                    <div className="divider"></div>
+                    <div className="divider">👌</div>
                     <div className="w-full flex-col lg:flex-row items-center lg:justify-evenly lg:items-start flex gap-4 p-4">
-                        <div className='prose'>
+                        <div className='prose self-start'>
                             <h1 className="text-2xl font-bold">Giới thiệu</h1>
-                            <p className={`min-h-40${isHideDetails ? "line-clamp-3" : ""}`}>{bookDetails.desc}</p>
+                            <p className={`${isHideDetails ? "line-clamp-3" : ""}`}>{bookDetails.desc}</p>
                             <button onClick={() => setIsHideDetails(!isHideDetails)} className="btn btn-outline btn-accent btn-sm">{isHideDetails ? 'Xem thêm' : 'Thu gọn'}</button>
                         </div>
                         <div className=" space-y-4 prose">
                             <h1 className="text-2xl font-bold">Nội dung tương tự</h1>
-                            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-full  not-prose">
+                            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-full not-prose">
                                 {
                                     booksData.books.map((book, index) => (
                                         <div key={index} className="flex items-center justify-center flex-col w-full">
@@ -166,7 +164,6 @@ const NovelsPage = () => {
                                                 <div className='title'>
                                                     <p className='font-bold cursor-pointer hover:text-orange-500 text-xs text-center line-clamp-2'>{book.name}</p>
                                                 </div>
-
                                             </div>
                                             <div className="italic">
                                                 {
@@ -177,138 +174,49 @@ const NovelsPage = () => {
                                                     ))
                                                 }
                                             </div>
-
                                         </div>
                                     ))
                                 }
                             </div>
-
                         </div>
                     </div>
-                    <div className="bg-base-200 p-4 flex justify-evenly items-center h-40">
+                    <div className="divider">💕</div>
+                    <div className="flex flex-col items-center">
+                        <h1 className="text-2xl font-bold p-4">Danh sách chương</h1>
+                        <div className="grid grid-cols-1 lg:grid-cols-2 justify-evenly p-4 bg-base-200 rounded-box gap-x-8 gap-y-2">
+                            {
+                                bookDetails.chapters.length > 0 ?
+
+                                    bookDetails.chapters.map((chapter, index) => (
+                                        <Link to={`/novels/${bookDetails.path}/${chapter.path}`} key={index} className="w-full prose hover:bg-base-100 p-4 rounded-box">
+                                            <div className="">
+                                                <div className="flex justify-start gap-x-2 items-center">
+                                                 
+                                                    <h6 className="text-lg font-bold text-primary">Chương {index + 1}:</h6>
+                                                    <p className="text-sm line-clamp-1">{chapter.title}</p>
+                                                </div>
+                                            </div>
+                                            <p className="text-sm line-clamp-2">{chapter.desc}</p>
+                                        </Link>
+                                    ))
+
+                                    : <div className="prose">
+                                        <blockquote className="font-bold">
+                                            "Hiện tại chưa có chương nào"
+                                        </blockquote>
+                                    </div>
+
+
+                            }
+                        </div>
+                    </div>
+
+                    <div className="divider">😘</div>
+
+                    <div className="bg-base-200 p-4 flex justify-evenly items-center h-40 my-4">
                         <Ads />
                     </div>
-                    <div className='mx-auto max-w-xl lg:max-w-4xl rounded-box bg-base-200 md:p-4'>
-                        <Tabs>
-                            <TabList className={
-                                `flex items-center justify-evenly gap-4 p-4 bg-base-100 rounded-box shadow-lg`
 
-                            }>
-                                <Tab className={
-                                    `cursor-pointer text-center focus:text-info text-lg font-semibold bg-transparent`
-                                }>Danh sách chương</Tab>
-                                <Tab className={
-                                    `cursor-pointer text-center focus:text-info text-lg font-semibold bg-transparent`
-                                }>Bình luận</Tab>
-                                <Tab className={
-                                    `cursor-pointer text-center focus:text-info text-lg font-semibold bg-transparent`
-                                }>Đánh giá</Tab>
-                            </TabList>
-                            <TabPanel className={
-                                `p-4 bg-base-200 rounded-box shadow-lg`
-                            }>
-                                <div className='space-y-4'>
-                                    <div className='flex items-center justify-between'>
-                                        <div className='flex flex-col'>
-                                            <div className='title'>
-                                                <p className='font-bold'>{bookDetails.lastChapter} chương</p>
-                                            </div>
-                                            <div>
-                                                <p>Truyện phát hành mỗi ngày!</p>
-                                            </div>
-                                        </div>
-                                        <div onClick={toggleIcon}>
-                                            <div className='cursor-pointer flex items-center gap-3'>
-                                                <p className='hidden md:block font-semibold'>Sắp xếp</p>
-                                                {isUpNarrow ? <ArrowUpNarrowWide /> : <ArrowDownWideNarrow />}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-col">
-                                        {bookDetails.chapters?.map((book, index) => (
-                                            <div key={index}>
-                                                <div className="flex items-center p-4 mb-4 w-full">
-                                                    <div className="img mr-4">
-                                                        {/* <img src={book.img} alt={book.name + 'Cover'} className='rounded-md w-20 h-auto object-cover cursor-pointer' /> */}
-                                                    </div>
-                                                    <div className="info text-left flex-grow">
-                                                        <div className='title'>
-                                                            <p className='font-bold cursor-pointer hover:text-orange-500'>{book.title}</p>
-                                                        </div>
-                                                        <div className="date italic">
-                                                            <p>{book.released}</p>
-                                                        </div>
-                                                    </div>
-                                                    <div className="price flex items-center cursor-pointer">
-                                                        <p>Miễn phí</p>
-                                                    </div>
-                                                </div>
-                                                <div className="divider"></div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </div>
-                            </TabPanel>
-                            <TabPanel className={
-                                `p-4 bg-base-200 rounded-box shadow-lg`
-                            }>
-                                <div className='space-y-4'>
-                                    <div className='flex items-center w-full'>
-                                        <div className="avatar mr-4">
-                                            <div className="w-12 h-12 rounded-full overflow-hidden">
-                                                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt='Avatar' className="w-full h-full object-cover" />
-                                            </div>
-                                        </div>
-                                        <textarea placeholder="Nhập bình luận của bạn tại đây." className="textarea textarea-accent textarea-bordered textarea-sm w-full" ></textarea>
-                                        <div className='send ml-4'>
-                                            <button className="btn btn-outline btn-accent">Gửi</button>
-                                        </div>
-                                    </div>
-                                    <div className='divider'></div>
-                                    <div className='w-full space-y-4'>
-                                        {userData.users.map((user, index) => (
-                                            <div className="chat chat-start" key={index}>
-                                                <div className="chat-image avatar">
-                                                    <div className="w-10 rounded-full">
-                                                        <img alt="Tailwind CSS chat bubble component" src={user.avatar} />
-                                                    </div>
-                                                </div>
-                                                <div className="chat-header ">
-                                                    <div className="chat-name font-semibold">
-                                                        {user.name}
-                                                    </div>
-                                                </div>
-                                                <div className="chat-bubble">{user.contentComment}</div>
-                                                <div className='chat-footer flex items-center gap-4'>
-                                                    <time className="text-xs opacity-50">{user.dateComment}</time>
-                                                    <div className="chapter text-xs text-gray-500">
-                                                        {user["chapter-comment"]}
-                                                    </div>
-                                                    <p>Thích</p>
-                                                    <p>Trả lời</p>
-                                                    <div className="dropdown dropdown-bottom">
-                                                        <div tabIndex={0} role="button" className="btn btn-circle btn-ghost btn-xs m-1">
-                                                            <MoreHorizontal size={12} className='block align-middle' />
-                                                        </div>
-                                                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32 md:w-52">
-                                                            <li><a>Báo cáo</a></li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </div>
-                            </TabPanel>
-                            <TabPanel className={
-                                `p-4 bg-base-200 rounded-box shadow-lg`
-                            }>
-                                <div>
-                                    <h1>Đánh giá</h1>
-                                </div>
-                            </TabPanel>
-                        </Tabs>
-                    </div>
                 </div>
             )}
         </div>
--- src/components/pages/routes.tsx
+++ src/components/pages/routes.tsx
@@ -21,7 +21,7 @@ const routes: Route[] = [
     },
     {
         path: '/novels',
-        name: 'novels',
+        name: 'genres',
         element: <GenrePage />,
     },
     {
